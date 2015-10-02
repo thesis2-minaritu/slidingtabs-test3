e4673357cb495eed19f437c6c40bf2584cb4c7a9
thesis2-minaritu <thesis2.minaritu@gmail.com>
2015-10-03 00:37:21 +0800

    sliding tabs
    sliding tabs, no problem in build, no deprecated apis. still can't run

---
+++ tabstest/app/src/main/java/com/mikhaella/tabstest/MainActivity.java
@@ -0,0 +1,25 @@
+package com.mikhaella.tabstest;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(),
+                MainActivity.this));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+}
